d2e80a02764f7d5b8251a0003d114a473b4ad6bf
caoxuexi <969718359@qq.com>
2021-08-24 12:55:48 +0800

    init

---
+++ src/main/java/com/caostudy/WikiApplication.java
@@ -0,0 +1,16 @@
+package com.caostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Cao Study
+ * @description WikiApplication
+ * @date 2021/8/24 12:40
+ */
+@SpringBootApplication
+public class WikiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+}
