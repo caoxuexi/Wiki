eb9acfba6fed9cc0e4682980d133a0610dda9f52
caoxuexi <969718359@qq.com>
2021-08-27 09:46:17 +0800

    电子书布局修改

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -10,7 +10,7 @@
         <a-sub-menu key="sub1">
           <template #title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 subnav 1
               </span>
           </template>
@@ -22,7 +22,7 @@
         <a-sub-menu key="sub2">
           <template #title>
               <span>
-                <laptop-outlined />
+                <laptop-outlined/>
                 subnav 2
               </span>
           </template>
@@ -34,7 +34,7 @@
         <a-sub-menu key="sub3">
           <template #title>
               <span>
-                <notification-outlined />
+                <notification-outlined/>
                 subnav 3
               </span>
           </template>
@@ -48,17 +48,81 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <a-list item-layout="vertical" size="large" :grid="{gutter:20,column:3}"
+              :pagination="pagination" :data-source="ebooks">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">  <!--它会将data-source项拿出来命名为item-->
+          <a-list-item key="item.name">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios'
+import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
+
+const pagination = {
+  onChange: page => {
+    console.log(page);
+  },
+  pageSize: 3, //一页显示三个
+};
 
 export default {
   name: 'Home',
   components: {
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
+  },
+
+  mounted() {
+    axios.get("http://localhost:8091/ebook/list?name=Spring").then((response) => {
+      console.log(response)
+      this.ebooks = response.data.content
+    })
+  },
+  data() {
+    return {
+      actions: [
+        {
+          type: 'StarOutlined',  //星星
+          text: '156',
+        },
+        {
+          type: 'LikeOutlined',  //点赞
+          text: '156',
+        },
+        {
+          type: 'MessageOutlined', //评论
+          text: '2',
+        },
+      ],
+      pagination: pagination,
+      ebooks:""
+    }
   }
 }
 </script>
