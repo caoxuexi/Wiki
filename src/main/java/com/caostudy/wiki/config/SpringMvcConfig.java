fb68b0d49265ec19e25025f2b8f4e97f880219f1
caoxuexi <969718359@qq.com>
2021-08-28 10:09:41 +0800

    增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器、拦截器

--- src/main/java/com/caostudy/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/caostudy/wiki/config/SpringMvcConfig.java
@@ -1,6 +1,6 @@
 package com.caostudy.wiki.config;
 
-import com.caostudy.wiki.Interceptor.LogInterceptor;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,13 +10,5 @@ import javax.annotation.Resource;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-    @Resource
-    LogInterceptor logInterceptor;
 
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**");
-    }
 }
