32b3683f6b8644481fcccbdbd337fd8f90c8cfce
caoxuexi <969718359@qq.com>
2021-08-28 15:01:16 +0800

    继承PageHelper插件，将mapper的sql日志打开

--- src/main/java/com/caostudy/wiki/service/impl/EbookServiceImpl.java
+++ src/main/java/com/caostudy/wiki/service/impl/EbookServiceImpl.java
@@ -8,6 +8,10 @@ import com.caostudy.wiki.resp.EbookQueryResp;
 import com.caostudy.wiki.service.EbookService;
 import com.caostudy.wiki.utils.CopyUtil;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -25,6 +29,8 @@ import java.util.List;
  */
 @Service
 public class EbookServiceImpl implements EbookService {
+    private static final Logger LOG= LoggerFactory.getLogger(EbookServiceImpl.class);
+
     @Autowired
     private EbookMapper ebookMapper;
 
@@ -35,7 +41,12 @@ public class EbookServiceImpl implements EbookService {
         if (!StringUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo=new PageInfo<>(ebookList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
+
         List<EbookQueryResp> respList = CopyUtil.copyList(ebookList, EbookQueryResp.class);
         return respList;
     }
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -4,9 +4,116 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <div class="about">
-        <h1>电子书管理</h1>
-      </div>
+      <a-table
+          :columns="columns"
+          :row-key="record => record.id"
+          :data-source="ebooks"
+          :pagination="pagination"
+          :loading="loading"
+          @change="handleTableChange"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar"/>
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary">
+              编辑
+            </a-button>
+            <a-button type="danger">
+              删除
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
     </a-layout-content>
   </a-layout>
 </template>
+
+<script>
+import axios from "axios";
+
+const columns = [
+  {
+    title: '封面',
+    dataIndex: 'cover',
+    slots: {customRender: 'cover'}
+  },
+  {
+    title: '名称',
+    dataIndex: 'name'
+  },
+  {
+    title: '分类',
+    slots: {customRender: 'category'}
+  },
+  {
+    title: '文档数',
+    dataIndex: 'docCount'
+  },
+  {
+    title: '阅读数',
+    dataIndex: 'viewCount'
+  },
+  {
+    title: '点赞数',
+    dataIndex: 'voteCount'
+  },
+  {
+    title: '操作',
+    key: 'action',
+    slots: {customRender: 'action'}
+  }
+];
+
+
+export default {
+  name: 'AdminEbook',
+  components: {},
+  methods: {
+    handleQuery(params) {
+      this.loading = true;
+      axios.get("/ebook/list", {
+        params: {
+          page: params.page,
+          size: params.size,
+          name: this.param.name
+        }
+      }).then((response) => {
+        this.loading = false;
+        const data = response.data;
+        this.ebooks = data.content;
+        // 重置分页按钮
+        this.pagination.current = params.page;
+        this.pagination.total = data.content.total;
+      });
+    },
+    /**
+     * 表格点击页码时触发
+     */
+    handleTableChange(pagination) {
+      console.log("看看自带的分页参数都有啥：" + pagination);
+      this.handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    }
+  },
+  mounted() {
+    this.handleQuery({})
+  },
+  data() {
+    return {
+      columns: columns,
+      pagination: {
+        current: 1,
+        pageSize: 10,
+        total: 0
+      },
+      loading: false,
+      ebooks: "",
+      param: "",
+    }
+  }
+}
+</script>
