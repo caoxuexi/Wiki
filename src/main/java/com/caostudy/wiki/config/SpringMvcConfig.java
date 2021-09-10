35d2ec0e27741ffbfe701e9988b15522515de709
caoxuexi <969718359@qq.com>
2021-09-10 21:43:30 +0800

    前后端登录校验(token校验)

--- src/main/java/com/caostudy/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/caostudy/wiki/config/SpringMvcConfig.java
@@ -1,6 +1,8 @@
 package com.caostudy.wiki.config;
 
 
+import com.caostudy.wiki.Interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +11,25 @@ import javax.annotation.Resource;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    LoginInterceptor loginInterceptor;
 
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+    }
 
 }
--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-footer style="text-align: center">
-    文档协同开发 {{user.name}}
+    文档协同开发<span v-show="user.id">，欢迎：{{user.name}}</span>
   </a-layout-footer>
 </template>
 
@@ -14,6 +14,5 @@
         return store.state.user
       }
     }
-
   };
 </script>
